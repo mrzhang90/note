a755b0f78dfb95fae1fc3dadfa421b24d91cf691
mrzhang90 <sp6688@vip.qq.com>
2018-04-01 23:35:21 +0800

    sleep

---
+++ js/es6/1-sleep.js
@@ -0,0 +1,10 @@
+async function test() {
+	console.log('Hello');
+	await sleep(1000);
+	console.log('world!') 
+} 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms))
+}
+test()
+ 
